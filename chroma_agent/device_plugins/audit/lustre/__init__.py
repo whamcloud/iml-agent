272bd85a7f05e8e784e909ba27f627c26507c75a
Valentine Kiselev <mrexox@outlook.com>
2019-06-17 18:13:32 +0300

    Don't loose job stats for one job on multiple targets. (#102)
    * Don't loose job stats for one job on multiple targets.
    
    When one job is reading/writing on multiple targets the job statistics
    are lost for some targets, if timestamps do not differ.
    
    When using `target_name + job_id` binding as a key for job's snapshot
    time, it becomes more common for target and don't eat much memory
    though.
    
    Signed-off-by: Valentine Kiselev <mrexox@outlook.com>
    
    * Reworked for better look and hierarchy
    
    Signed-off-by: Kiselev Valentine <mrexox@outlook.com>
    
    * Applied AlexTalker suggestion on extra variables
    
    Signed-off-by: Valentine Kiselev <mrexox@outlook.com>

--- chroma_agent/device_plugins/audit/lustre/__init__.py
+++ chroma_agent/device_plugins/audit/lustre/__init__.py
@@ -430,7 +430,7 @@ class ObdfilterAudit(TargetAudit):
                 "tot_pending": "tot_pending",
             }
         )
-        self.job_stat_last_snapshot_time = defaultdict(int)
+        self.job_stat_last_snapshot_time = defaultdict(lambda: defaultdict(int))
 
     def get_brw_stats(self, target):
         """Return a dict representation of an OST's brw_stats histograms."""
@@ -565,7 +565,7 @@ class ObdfilterAudit(TargetAudit):
 
         #  Initialize this dict in preparation to collect just these latest snapshot
         #  times as seen in this stats sample
-        latest_job_stat_snapshot_times = defaultdict(int)
+        latest_job_stat_snapshot_times = defaultdict(lambda: defaultdict(int))
 
         stats_to_return = []
         for stat in stats:
@@ -573,11 +573,15 @@ class ObdfilterAudit(TargetAudit):
             stat["read"] = stat.pop("read_bytes")
             stat["write"] = stat.pop("write_bytes")
 
+            job_id = stat["job_id"]
+            snapshot_time = stat["snapshot_time"]
+
+            #  Tagging by target name and job_id so the one job for different targets is not ignored
             #  Record that we know about this stat
-            latest_job_stat_snapshot_times[stat["job_id"]] = stat["snapshot_time"]
+            latest_job_stat_snapshot_times[target_name][job_id] = snapshot_time
 
             #  if we knew about this last run, and the time is new, then report it
-            if self.job_stat_last_snapshot_time[stat["job_id"]] < stat["snapshot_time"]:
+            if self.job_stat_last_snapshot_time[target_name][job_id] < snapshot_time:
                 stats_to_return.append(stat)
 
         #  The local dict will have all the current times for jobs we are tracking, so update the instance copy.
